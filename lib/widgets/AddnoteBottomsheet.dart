810b05af0272308702ba4c8e67f9ddfe4851f996
Mostafaslima66 <mostafaslima61@gmail.com>
2024-12-11 20:43:14 +0200

    add addnote cubut to addnote bottomsheet (integrate cubit)

--- lib/widgets/AddnoteBottomsheet.dart
+++ lib/widgets/AddnoteBottomsheet.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:noteapp/cubits/Add_note_cubit/add_note_cubit.dart';
 import 'package:noteapp/widgets/Add_note_form.dart';
-
 
 class AddnoteBottomsheet extends StatelessWidget {
   const AddnoteBottomsheet({super.key});
@@ -13,11 +15,32 @@ class AddnoteBottomsheet extends StatelessWidget {
         right: 16,
         bottom: MediaQuery.of(context).viewInsets.bottom, // Adjust for keyboard
       ),
-      child: const SingleChildScrollView(
-        child: Addnoteform(),
-      ),
+      child: SingleChildScrollView(
+          child: BlocConsumer<AddNoteCubit, AddNoteState>(
+        listener: (context, state) {
+          if (state is AddNoteSuccess) {
+            // Dismiss the bottom sheet on success
+            Navigator.pop(context);
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text("Note added successfully!")),
+            );
+          } else if (state is AddNoteFailure) {
+            print("failed${state.errormessage}");
+            // Show error message on failure
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text("Failed to add note: ${state.errormessage}"),
+              ),
+            );
+          }
+        },
+        builder: (context, state) {
+          return ModalProgressHUD(
+            inAsyncCall: state is AddNoteLoading ? true : false,
+            child:const Addnoteform(),
+          );
+        },
+      )),
     );
   }
 }
-
-
