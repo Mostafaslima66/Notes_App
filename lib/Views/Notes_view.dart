e66603c9d55865eafe060c156210ba2f1d9fb46a
Mostafaslima66 <mostafaslima61@gmail.com>
2024-12-14 19:18:01 +0200

    create readnotes cubit method

--- lib/Views/Notes_view.dart
+++ lib/Views/Notes_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:noteapp/constants.dart';
+import 'package:noteapp/cubits/read%20notes%20cubit/read_notes_cubit.dart';
 import 'package:noteapp/widgets/AddnoteBottomsheet.dart';
 import 'package:noteapp/widgets/Notesview_body.dart';
 
@@ -8,33 +10,36 @@ class NotesView extends StatelessWidget {
   static String id = "Notes_view";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            isScrollControlled: true,
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(20),
+    return BlocProvider(
+      create: (context) => ReadNotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(20),
+                ),
               ),
+              builder: (BuildContext context) {
+                return AddnoteBottomsheet();
+              },
+            );
+          },
+          backgroundColor: kprimarycolor,
+          shape: const CircleBorder(),
+          child: const Center(
+            child: Icon(
+              Icons.add,
+              color: Colors.black,
+              size: 26,
             ),
-            builder: (BuildContext context) {
-              return AddnoteBottomsheet();
-            },
-          );
-        },
-        backgroundColor: kprimarycolor,
-        shape: const CircleBorder(),
-        child: const Center(
-          child: Icon(
-            Icons.add,
-            color: Colors.black,
-            size: 26,
           ),
         ),
+        body: const NotesviewBody(),
       ),
-      body: const NotesviewBody(),
     );
   }
 }
--- lib/cubits/read notes cubit/read_notes_cubit.dart
+++ lib/cubits/read notes cubit/read_notes_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:noteapp/constants.dart';
 import 'package:noteapp/models/Note_model.dart';
 
 part 'read_notes_state.dart';
 
-class ReadNotesCubitCubit extends Cubit<ReadNotesState> {
-  ReadNotesCubitCubit() : super(ReadNotesInitial());
+class ReadNotesCubit extends Cubit<ReadNotesState> {
+  ReadNotesCubit() : super(ReadNotesInitial());
+
+  readallnotes() {
+    try {
+      var notebox = Hive.box<NoteModel>(knotebox);
+      List<NoteModel> notes = notebox.values.toList();
+      emit(ReadNotesSuccess(notes));
+    } catch (e) {
+      emit(ReadNotesFailure(e.toString()));
+    }
+  }
 }
